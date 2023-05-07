b2c83c1ce2e48053c4a5c419f6d33e921e8a746d
drissakande <drissakande@gmail.com>
2023-05-07 18:46:59 -0400

    Write code in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
+// //
+//
+//
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const formId = document.querySelector("form");
+  formId.addEventListener("submit", function(e){
+  e.preventDefault();
+  const inputContent = document.querySelector("#new-task-description");
+
+  let listItem = document.createElement("li");
+  const btn = document.createElement("button");
+  btn.textContent = "X"
+  btn.addEventListener("click", deleteId);
+  listItem.textContent = `${inputContent.value}`;
+
+  const unOrderedList = document.querySelector("ul");
+    unOrderedList.appendChild(listItem);
+    unOrderedList.appendChild(btn);
+      formId.reset();
+
 });
+
+
+
+});
+function deleteId(e){
+  e.target.parentNode.remove();
+}
